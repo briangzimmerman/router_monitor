ace0c5a8af7ee85195d19dedfa565b98651b36d0
Brian Zimmerman <briangzimmerman@gmail.com>
2018-09-28 22:36:06 -0500

    doing stuff

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const port = 3030;
+const hbs = require('hbs');
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-
+hbs.registerPartials(`${__dirname}/views/partials`);
+app.set('view engine', 'hbs')
 app.use(express.static(`${__dirname}/public`));
 
 server.listen(port, () => {
@@ -13,4 +15,12 @@ server.listen(port, () => {
 
 io.on('connection', () => {
     console.log('User Connected.')
+});
+
+app.get('/', (req, res) => {
+    res.render('index.hbs');
+});
+
+app.get('/history', (req, res) => {
+    res.render('history.hbs');
 });
